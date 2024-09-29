c3f71c75f04407e193b00cbaf0c176c0160985e8
Matt <mank.matvei@gmail.com>
2024-09-29 17:05:07 +0300

    Update index.js

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import {memo} from 'react';
 import PropTypes from 'prop-types';
 import {cn as bem} from '@bem-react/classname';
 import {numberFormat} from '../../utils';
 import './style.css';
 import {Link} from "react-router-dom";
-=======
-import { memo, useState } from 'react';
-import PropTypes from 'prop-types';
-import { cn as bem } from '@bem-react/classname';
-import { numberFormat } from '../../utils';
-import './style.css';
->>>>>>> b5bf9380d97043e17bc3b5ea839ac797cd957969
 
 function Item(props) {
   const cn = bem('Item');
@@ -22,16 +14,11 @@ function Item(props) {
 
   return (
     <div className={cn()}>
-<<<<<<< HEAD
       <div className={cn('title')}>
         <Link className={'decor'} to={`/product/${props.item._id}`}>
           <div>{props.item.title}</div>
         </Link>
       </div>
-=======
-      {/*<div className={cn('code')}>{props.item._id}</div>*/}
-      <div className={cn('title')}>{props.item.title}</div>
->>>>>>> b5bf9380d97043e17bc3b5ea839ac797cd957969
       <div className={cn('actions')}>
         <div className={cn('price')}>{numberFormat(props.item.price)} ₽</div>
         <button onClick={callbacks.onAdd}>Добавить</button>
@@ -50,12 +37,8 @@ Item.propTypes = {
 };
 
 Item.defaultProps = {
-<<<<<<< HEAD
   onAdd: () => {
   },
-=======
-  onAdd: () => {},
->>>>>>> b5bf9380d97043e17bc3b5ea839ac797cd957969
 };
 
 export default memo(Item);
