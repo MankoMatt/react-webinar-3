f6b5426702be9084ccd90a34617b387bdae92f43
Matt <mank.matvei@gmail.com>
2024-09-29 17:02:57 +0300

    Update index.js

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {memo, useCallback} from 'react';
 import propTypes from 'prop-types';
 import PropTypes from 'prop-types';
@@ -24,26 +23,6 @@ function ItemBasket(props) {
           <div onClick={callbacks.closeModal}>{props.item.title}</div>
         </Link>
       </div>
-=======
-import { memo, useCallback } from 'react';
-import propTypes from 'prop-types';
-import { numberFormat } from '../../utils';
-import { cn as bem } from '@bem-react/classname';
-import PropTypes from 'prop-types';
-import './style.css';
-
-function ItemBasket(props) {
-  const cn = bem('ItemBasket');
-
-  const callbacks = {
-    onRemove: e => props.onRemove(props.item._id),
-  };
-
-  return (
-    <div className={cn()}>
-      {/*<div className={cn('code')}>{props.item._id}</div>*/}
-      <div className={cn('title')}>{props.item.title}</div>
->>>>>>> b5bf9380d97043e17bc3b5ea839ac797cd957969
       <div className={cn('right')}>
         <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
         <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
@@ -66,12 +45,8 @@ ItemBasket.propTypes = {
 };
 
 ItemBasket.defaultProps = {
-<<<<<<< HEAD
   onRemove: () => {
   },
-=======
-  onRemove: () => {},
->>>>>>> b5bf9380d97043e17bc3b5ea839ac797cd957969
 };
 
 export default memo(ItemBasket);
