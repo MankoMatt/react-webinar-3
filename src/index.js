410035b1e3a62d0c5d9cac267214bd9fb1c99748
Matt <mank.matvei@gmail.com>
2024-09-29 16:51:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import {createRoot} from 'react-dom/client';
 import App from './app';
 import Store from './store';
 import {StoreContext} from './store/context';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
-=======
-import { createRoot } from 'react-dom/client';
-import App from './app';
-import Store from './store';
-import { StoreContext } from './store/context';
->>>>>>> b5bf9380d97043e17bc3b5ea839ac797cd957969
 
 const store = new Store();
 
@@ -18,15 +11,9 @@ const root = createRoot(document.getElementById('root'));
 
 // Первый рендер приложения
 root.render(
-<<<<<<< HEAD
   <Router>
     <StoreContext.Provider value={store}>
         <App/>
     </StoreContext.Provider>
   </Router>
-=======
-  <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>,
->>>>>>> b5bf9380d97043e17bc3b5ea839ac797cd957969
 );
